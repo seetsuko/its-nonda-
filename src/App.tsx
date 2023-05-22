ca77ae23894fdec02ea1a0507671c5afbfc032c9
seetsuko <radgjms@gmail.com>
2023-05-23 02:50:13 +0900

    refactor: Counterコンポーネントの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './scss/App.scss';
 import { Header } from './Header';
+import { Counter } from './Counter';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Counter />
     </div>
   );
 };
