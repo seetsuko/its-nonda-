9ce6cd96e9b9203253ce61a3a4110c772bec762d
seetsuko <radgjms@gmail.com>
2023-05-23 16:33:42 +0900

    feat: onclickで前回からの経過時間を表示する機能を追加
    前回からの経過時間を表示するようにしてるが、途中。（動作していない）

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,12 +1,32 @@
 import React, { useState } from "react"
 import { Box,Text, Button } from '@chakra-ui/react'
+import {  differenceInMinutes,differenceInHours, differenceInSeconds } from "date-fns";
+import { format } from "date-fns-tz";
 
 export const Counter = () => {
-
+  const defaultDate = new Date().toString()
+  const [timeStamp, setTimeStamp] = useState(defaultDate);
   const [count, setCount] = useState(0)
 
+console.log(timeStamp)
   const onIncrement = () => {
+    const date = format(new Date(), 'yyyy-MM-dd HH:mm:ss xxx', { timeZone: 'Asia/Tokyo' })
     setCount(count + 1)
+    setTimeStamp(date)
+  }
+
+  const timeLimit = () =>{
+      const diffInMinutes = differenceInMinutes(new Date(),Date.parse(timeStamp));
+      const diffInHours = differenceInHours(new Date(),Date.parse(timeStamp));
+      const diffInSeconds = differenceInSeconds(new Date(),Date.parse(timeStamp));
+      const sec = diffInSeconds%60
+      const min = diffInMinutes%60
+      const hour = diffInHours%24
+      console.log(sec)
+    return(
+      <div><Text>時間：{hour}</Text>
+      <Text>分：{min}</Text></div>
+    )
   }
 
   return(
@@ -17,6 +37,10 @@ export const Counter = () => {
         textAlign='center'
         >
       <Text>カウント：{count}</Text>
+      <Text>時間：{}</Text>
+      {timeLimit()}
+      {/* <Text>時間：{hour}</Text>
+      <Text>分：{min}</Text> */}
       <Button
           colorScheme='blue'
           onClick={onIncrement}
