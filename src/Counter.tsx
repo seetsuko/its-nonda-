f2491d2dd75007e72d52faa88415147a1277c6ff
seetsuko <radgjms@gmail.com>
2023-05-23 09:48:13 +0900

    fix: カウンター機能を修正
    カウンター機能が未完成だったので、修正した。

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react"
+import { Box,Text, Button } from '@chakra-ui/react'
 
 export const Counter = () => {
 
   const [count, setCount] = useState(0)
 
+  const onIncrement = () => {
+    setCount(count + 1)
+  }
+
   return(
-    <div>
-      カウント：{count}
-    </div>
+    <Box
+        padding={10}
+        backgroundColor="red"
+        display='block'
+        >
+      <Text>カウント：{count}</Text>
+      <Button
+          colorScheme='blue'
+          onClick={onIncrement}
+      >Button</Button>
+    </Box>
   )
 }
