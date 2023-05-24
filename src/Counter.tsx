7601149dfa8f7fffc90564a41f5c4d21782c9f00
seetsuko <radgjms@gmail.com>
2023-05-24 14:26:27 +0900

    revert: 経過時間機能を削除
    アプリの構成としてまだ必要でないので、一旦もとに戻した。

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -4,29 +4,10 @@ import {  differenceInMinutes,differenceInHours, differenceInSeconds } from "dat
 import { format } from "date-fns-tz";
 
 export const Counter = () => {
-  const defaultDate = new Date().toString()
-  const [timeStamp, setTimeStamp] = useState(defaultDate);
   const [count, setCount] = useState(0)
 
-console.log(timeStamp)
-  const onIncrement = () => {
-    const date = format(new Date(), 'yyyy-MM-dd HH:mm:ss xxx', { timeZone: 'Asia/Tokyo' })
+  const onIncrement = () =>{
     setCount(count + 1)
-    setTimeStamp(date)
-  }
-
-  const timeLimit = () =>{
-      const diffInMinutes = differenceInMinutes(new Date(),Date.parse(timeStamp));
-      const diffInHours = differenceInHours(new Date(),Date.parse(timeStamp));
-      const diffInSeconds = differenceInSeconds(new Date(),Date.parse(timeStamp));
-      const sec = diffInSeconds%60
-      const min = diffInMinutes%60
-      const hour = diffInHours%24
-      console.log(sec)
-    return(
-      <div><Text>時間：{hour}</Text>
-      <Text>分：{min}</Text></div>
-    )
   }
 
   return(
@@ -38,9 +19,6 @@ console.log(timeStamp)
         >
       <Text>カウント：{count}</Text>
       <Text>時間：{}</Text>
-      {timeLimit()}
-      {/* <Text>時間：{hour}</Text>
-      <Text>分：{min}</Text> */}
       <Button
           colorScheme='blue'
           onClick={onIncrement}
