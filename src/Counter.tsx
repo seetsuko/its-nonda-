12869e8572440c144ba1c1e13afdd2dd6bb03fdf
seetsuko <radgjms@gmail.com>
2023-05-26 00:32:31 +0900

    feat: １秒ごとに時間が表示されるようにした

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
-import { Box, Text, Button } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Box, Text, Button, calc, Divider } from '@chakra-ui/react';
+import dayjs from 'dayjs';
 
 export const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState("");
 
-  const onIncrement = () => {
-    setCount(count + 200);
-  };
+  // 時計の関数
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCount(String(dayjs()));
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+    console.log(count)
 
-  const time = () => {
-
-  }
+    // ボタンを
 
   return (
     <Box padding={10} backgroundColor="red" display="block" textAlign="center">
       <Text>カウント：{count}ml</Text>
-      <Text>時間：{}</Text>
-      <Button colorScheme="blue" onClick={onIncrement}>
+      <Text>時間：</Text>
+      <Button colorScheme="blue">
         Button
       </Button>
     </Box>
