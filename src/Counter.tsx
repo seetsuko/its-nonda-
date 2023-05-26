7fa04225709d74eec98bff321c56d18ddc5dfed6
seetsuko <radgjms@gmail.com>
2023-05-27 02:47:20 +0900

    feat: ボタンを押した時のデータを取得するため、APIを叩く機能を追加

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,11 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Text, Button, calc, Divider } from '@chakra-ui/react';
 import dayjs from 'dayjs';
+import axios, { AxiosResponse } from "axios"
+
+type Artical = {
+  id: string;
+  amount: number;
+  time: string;
+}
+
+const getArticals = async (): Promise<Artical[]> => {
+  try{
+    const url = "http://localhost:3100/timer"
+    const response = await axios.get<Artical[]>(url)
+    console.log(response)
+    return response.data
+  } catch(error) {
+    console.error(error);
+    return[]
+  }
+}
 
 export const Counter = () => {
+  const [timerData, setTimerData] = useState<[] |Artical[]>([]);
   const [count, setCount] = useState(0);
   const [timeStamp, setTimeStamp] = useState("");
   const min = Math.floor(count/60)
+
+
+    useEffect(() =>{
+      (async () =>{
+        const data =await getArticals();
+        setTimerData(data)
+      })()
+    },[]);
+
+    console.log(timerData)
 
     const increment = () => {
       const now = dayjs().format("YYYY-MM-DDTHH:mm:ss")
@@ -25,6 +55,7 @@ export const Counter = () => {
       <Button colorScheme="blue" onClick={increment}>
         Button
       </Button>
+      {/* <div>{timerData}</div> */}
     </Box>
   );
 };
