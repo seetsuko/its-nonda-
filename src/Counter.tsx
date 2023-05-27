34ac65a4c77a6ac2a6da89bc2ddd3f51ecd7d51c
seetsuko <radgjms@gmail.com>
2023-05-27 22:22:28 +0900

    feat: ボタンを押すと前回押した時間からの経過時間が表示されるようにした。

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -14,8 +14,8 @@ export const Counter = () => {
   const [drinkData, setDrinkData] = useState([]);
   const [drinkTime, setDrinkTime] = useState('');
   const [count, setCount] = useState(0);
-  const [timeStamp, setTimeStamp] = useState('');
-  const min = Math.floor(count / 60);
+  const [elapsedTime, setElapsedTime] = useState('');
+  const [ running,setRunning] = useState(false)
 
   useEffect(() => {
     axios
@@ -29,25 +29,40 @@ export const Counter = () => {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+
+    console.log("スタート・ストップが切り替わりました。")
+    if(running){
+      const interval = setInterval(() => {
+        const now = dayjs().format('YYYY-MM-DDTHH:mm:ss');
+        const diffHour = dayjs(now).diff(dayjs(drinkTime), 'hour');
+        const diffMin = dayjs(now).diff(dayjs(drinkTime), 'minute');
+        const diffSec = dayjs(now).diff(dayjs(drinkTime), 'second');
+
+        setElapsedTime(`${diffHour}時間${diffMin % 60}分${diffSec % 60}秒`);
+        setCount((c) => c + 1);
+      }, 1000);
+      return () => {
+        clearInterval(interval)
+      }
+    }
+  }, [running]);
 
   console.log(drinkTime);
 
   const increment = () => {
-    const now = dayjs().format('YYYY-MM-DDTHH:mm:ss');
-    setTimeStamp(now);
-    const interval = setInterval(() => {
-      setCount((c) => c + 1);
-    }, 1000);
-    return () => clearInterval(interval);
-  };
+      setRunning(true)
+      setElapsedTime("")
+}
+
+  console.log(elapsedTime);
 
   return (
     <Box padding={10} backgroundColor="red" display="block" textAlign="center">
       <Text>
-        カウント：{min}：{count}
+        {drinkTime}
+        カウント：{count}
       </Text>
-      <Text>時間：{timeStamp}</Text>
+      <Text>時間：{elapsedTime}</Text>
       <Button colorScheme="blue" onClick={increment}>
         Button
       </Button>
@@ -62,7 +77,6 @@ export const Counter = () => {
             );
           })}
         </ul>
-        {drinkTime}
       </div>
     </Box>
   );
