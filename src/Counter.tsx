1069ce9c74fd04071dfcd8870f96231ab3562310
seetsuko <radgjms@gmail.com>
2023-05-27 20:39:16 +0900

    refactor: setDrinkTimeに最新の記録時間が入るように修正した。

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,54 +1,51 @@
 import React, { SetStateAction, useEffect, useState } from 'react';
 import { Box, Text, Button, calc, Divider } from '@chakra-ui/react';
 import dayjs from 'dayjs';
-import axios, { AxiosResponse } from "axios"
+import axios, { AxiosResponse } from 'axios';
 
 type Artical = {
   id: string;
   amount: number;
   time: string;
-}
-const urlAPI = "http://localhost:3100/timer"
-
+};
+const urlAPI = 'http://localhost:3100/timer';
 
 export const Counter = () => {
   const [drinkData, setDrinkData] = useState([]);
-  const [drinkTime,setDrinkTime] = useState("")
+  const [drinkTime, setDrinkTime] = useState('');
   const [count, setCount] = useState(0);
-  const [timeStamp, setTimeStamp] = useState("");
-  const min = Math.floor(count/60)
+  const [timeStamp, setTimeStamp] = useState('');
+  const min = Math.floor(count / 60);
 
+  useEffect(() => {
+    axios
+      .get(urlAPI)
+      .then((res) => {
+        setDrinkData(res.data);
+        res.data.map((data: Artical) => {
+          return setDrinkTime(data.time);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(()=>{
-      axios.get(urlAPI)
-    .then((res)=>{
-      console.log(res)
-      setDrinkData(res.data)
-    })
-    .catch((error)=> {
-      console.log(error);
-    })
-    },[]);
+  console.log(drinkTime);
 
-    // console.log(drinkTime)
-
-    const increment = () => {
-      const now = dayjs().format("YYYY-MM-DDTHH:mm:ss")
-      setTimeStamp(now)
-      const interval = setInterval(() => {
-        setCount(c => c + 1);
-      }, 1000);
-      return () => clearInterval(interval);
-    }
-
-    const test = (data:SetStateAction<string>) =>{
-      setDrinkTime(data)
-    }
+  const increment = () => {
+    const now = dayjs().format('YYYY-MM-DDTHH:mm:ss');
+    setTimeStamp(now);
+    const interval = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  };
 
   return (
     <Box padding={10} backgroundColor="red" display="block" textAlign="center">
-      <Text>カウント：{min}：
-      {count}
+      <Text>
+        カウント：{min}：{count}
       </Text>
       <Text>時間：{timeStamp}</Text>
       <Button colorScheme="blue" onClick={increment}>
@@ -56,13 +53,16 @@ export const Counter = () => {
       </Button>
       <div>
         <ul>
-          {drinkData.map((data:Artical)=>{
-            test(data.time)
-            return(
-            <li key={data.id.toString()}><p>{data.time}</p><p>{data.amount}ml</p></li>)
-})}
+          {drinkData.map((data: Artical) => {
+            return (
+              <li key={data.id}>
+                <p>{data.time}</p>
+                <p>{data.amount}ml</p>
+              </li>
+            );
+          })}
         </ul>
-        {/* {timerData} */}
+        {drinkTime}
       </div>
     </Box>
   );
