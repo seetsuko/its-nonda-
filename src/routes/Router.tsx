440ebc70cc1405773a46061f433c377b88131107
seetsuko <radgjms@gmail.com>
2023-06-02 01:58:05 +0900

    feat: firebaseを使って新規登録機能追加

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from '../user/Login';
-import { Register } from '../user/Register';
 import { UserEdit } from '../user/UserEdit';
-import { Counter } from '../pages/Counter';
+// import { Counter } from '../pages/Counter';
 import { Mypage } from '../user/Mypage';
+import { Register } from '../user/Register';
 
 export const Router = () => {
   return (
--- src/user/Register.tsx
+++ src/user/Register.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+
 import { FormLabel, Input, Button, VStack, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import useCookies from 'react-cookie/cjs/useCookies';
 import { useDispatch } from 'react-redux';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { signIn } from '../redux/authSlice';
 import { url } from '../const';
 import { useSelector } from '../redux/store';
+import { auth } from "../FirebaseConfig";
+
+/* ↓ログインしているかどうかを判定する */
 
 export const Register = () => {
-  const auth = useSelector((state) => state.auth.isSignIn);
+  const uth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
   const navigation = useNavigate();
   const {
@@ -22,49 +27,45 @@ export const Register = () => {
   const [errorMessage, setErrorMessage] = useState('');
 
   const onSubmit = async (data: any) => {
-    let token = '';
-    await axios
-      .post(`${url}/users`, data)
-      .then((res) => {
-        console.log(res);
-        token = res.data.token;
-        setCookie('token', token);
-        dispatch(signIn(true));
-        navigation('/');
-      })
-      .catch((err) => {
-        setErrorMessage(`ユーザー登録に失敗しました。 ${err}`);
-      });
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+    } catch(error) {
+      alert("正しく入力してください");
+    }
   };
 
   return (
     <div className="main">
-      {auth && <Navigate to="/" />}
+      {uth && <Navigate to="/" />}
       <h2> 新規登録</h2>
       <p className="error">{errorMessage}</p>
       <VStack>
         <form onSubmit={handleSubmit(onSubmit)} className="user-form">
           <div>
-            <FormLabel htmlFor="email">メールアドレス</FormLabel>
+            <FormLabel htmlFor="email">メールアドレス
             <Input
               id="email"
               placeholder="メールアドレス"
               // バリデーション
               {...register('email', { required: true })}
-            />
+            /></FormLabel>
             {errors.email && (
               <Text color="red.400">メールアドレスを入力してください</Text>
             )}
           </div>
           <div>
-            <FormLabel htmlFor="password">パスワード</FormLabel>
+            <FormLabel htmlFor="password">パスワード
             <Input
               type="password"
               id="login-password"
               placeholder="パスワード"
               // バリデーション
               {...register('password', { required: true })}
-            />
+            /></FormLabel>
             {errors.password && (
               <Text color="red.400">パスワードを入力してください</Text>
             )}
@@ -78,6 +79,5 @@ export const Register = () => {
         </form>
       </VStack>
     </div>
-      
   );
 };
