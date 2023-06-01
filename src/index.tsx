219a5aa5963de6d17a314a2dd666f1213dbd0015
seetsuko <radgjms@gmail.com>
2023-06-01 13:05:38 +0900

    feat: ログインの状態管理のためreduxでスライスを作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/index.scss';
+import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+
+import { CookiesProvider } from 'react-cookie';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+     <BrowserRouter>
+    <Provider store={store}>
+    <CookiesProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+      </CookiesProvider>
+    </Provider>
+    </BrowserRouter>
   </ChakraProvider>
 );
