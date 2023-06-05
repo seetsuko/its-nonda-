870ca4645867cd5f98598d09aa26bee40b6d4c78
seetsuko <radgjms@gmail.com>
2023-06-05 22:06:04 +0900

    refactor: 不要なコンポーネントを削除した

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from '../user/Login';
-import { UserEdit } from '../user/UserEdit';
 import { Register } from '../user/Register';
 import { Header } from '../Header';
 import { Counter } from '../pages/Counter';
@@ -15,7 +14,6 @@ export const Router = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<UserEdit />} />
         <Route path="/" element={<Counter />} />
         <Route path="/timelist" element={<TimeList />} />
       </Routes>
