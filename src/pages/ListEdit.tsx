069168edd3743d058fa08a46bed7e2b8d67d41de
seetsuko <radgjms@gmail.com>
2023-06-25 03:08:40 +0900

    feat: リストのdelete機能を追加した

--- src/pages/ListEdit.tsx
+++ src/pages/ListEdit.tsx
@@ -2,14 +2,19 @@ import React, { useContext } from 'react';
 import { Box, Button, FormLabel, Input, Text, VStack } from '@chakra-ui/react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
-import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from 'react-router-dom';
 import { LoginStatusContext } from '../App';
 import { url } from '../const';
 
 export const ListEdit = () => {
-  const id =useParams()
-  const {state} = useLocation()
-  const title =state.selectTitle
+  const id = useParams();
+  const { state } = useLocation();
+  const title = state.selectTitle;
   const { loading, token } = useContext(LoginStatusContext);
   const navigate = useNavigate();
   const {
@@ -20,10 +25,9 @@ export const ListEdit = () => {
 
   const login = token !== '';
   // console.log(id.listId);
-  console.log(title)
+  console.log(title);
 
-
-  const onCreateSubmit = async (data: any) => {
+  const onEditList = async (data: any) => {
     await axios
       .put(`${url}/do_lists/${id.listId}`, data, {
         headers: {
@@ -37,6 +41,20 @@ export const ListEdit = () => {
       });
   };
 
+  const onDeleteList = () => {
+    axios
+      .delete(`${url}/do_lists/${id.listId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log('削除完了');
+        navigate('/');
+      });
+  };
+
   return (
     <Box w="90vw">
       {!loading && (
@@ -45,10 +63,7 @@ export const ListEdit = () => {
             <Box>
               <Text fontSize="xl"> リスト編集</Text>
               <VStack>
-                <form
-                  onSubmit={handleSubmit(onCreateSubmit)}
-                  className="user-form"
-                >
+                <form onSubmit={handleSubmit(onEditList)} className="user-form">
                   <Box>
                     <FormLabel htmlFor="title">
                       リスト名
@@ -65,10 +80,20 @@ export const ListEdit = () => {
                   </Box>
                   <Box>
                     <Button mt={4} colorScheme="teal" type="submit">
-                      編集！
+                      編集する
                     </Button>
                   </Box>
                 </form>
+                <Box>
+                  <Button
+                    mt={4}
+                    colorScheme="red"
+                    type="submit"
+                    onClick={onDeleteList}
+                  >
+                    削除する
+                  </Button>
+                </Box>
               </VStack>
             </Box>
           ) : (
